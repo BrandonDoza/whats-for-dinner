ee522fe76bd2522069782a649fdcfad028a56677
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2023-12-22 09:33:06 -0700

    Define the food group arrays

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// Foods
+var sides = [
+    'French Fries', 'Mashed Potatoes', 'Green Beans',
+    'Broccoli', 'Sweet Potatoes', 'Sweet Corn', 'Spanish Rice',
+    'Brussels Sprouts', 'Garlic Bread', 'Baked Potato', 'Mac N\' Cheese',
+    'Cole Slaw', 'Baked Beans'
+]
+
+var mains = [
+    'New York Strip', 'Grilled Chicken', 'Salmon Filet', 'Elk Medallions',
+    'Halibut', 'Bratwurst', 'BBQ Shrimp', 'Lamb Kabab', 'Grilled Tofu', 
+    'Turkey Burger', 'Fried Chicken', 'Pork Chops', 'Beer Batered Cod'
+]
+
+var deserts = [
+    'Pumpkin Pie', 'Strawberry Shortcake', 'Vanilla Ice Cream',
+    'Chocolate Cake', 'Banana Pudding', 'Chocolate Chip Cookies',
+    'Apple Pie', 'Brownies', 'Pistachio Ice Cream', 'Popcorn', 
+    'Skittles', 'Donuts', 'Cherry Pie'
+]
