2d7db71610d53727b1262e191feeb0ad8c6d7ecd
Brandon Doza <brandondoza@Brandos-M2-MacBook-Pro.local>
2023-12-24 13:13:26 -0700

    Add corrected js eventListeners to the radio buttons

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 // query selectors
-// var sideRadio = document.querySelector('#side');
+var sideRadio = document.querySelector('#side');
 var mainRadio = document.querySelector('#main-dish');
 var desertRadio = document.querySelector('#desert');
-// may not need querySelectorAll
-var selectedRadio = document.querySelectorAll('input[name="food"]:checked');
 var letsCookBtn = document.querySelector('.lets-cook');
+// may not need querySelectorAll
+// var selectedRadio = document.querySelectorAll('input[name="food"]:checked');
 // var addRecipeBtn = document.querySelector('button');
 // var outputContainer = document.querySelector('.output');
 
-//event listeners
+// variables to store the checked state of radio buttons
+var isSideChecked = false;
+var isMainChecked = false;
+var isDesertChecked = false;
 
-letsCookBtn.addEventListener('click', getRandomFood);
-// sideRadio.addEventListener('change', randomSide);
-// mainRadio.addEventListener('change', randomMain);
-// desertRadio.addEventListener('change', randomDesert);
+//event listeners
+sideRadio.addEventListener('change', function() {
+    isSideChecked = sideRadio.checked;
+});
+mainRadio.addEventListener('change', function() {
+    isMainChecked = mainRadio.checked;
+});
+desertRadio.addEventListener('change', function() {
+    isDesertChecked = desertRadio.checked;
+});
+letsCookBtn.addEventListener('click', function() {
+    if (isSideChecked || isMainChecked || isDesertChecked) {
+    if (isSideChecked) {
+        randomSide();
+    } else if (isMainChecked) {
+        randomMain();
+    } else if (isDesertChecked) {
+        randomDesert();
+    } else {
+        alert("you must choose one")
+    }
+    }
+});
 
 
 // Foods
@@ -38,28 +60,30 @@ var deserts = [
 ]
 
 // functions
+function getRandomFood(array) {
+    var i =  Math.floor(Math.random() * (array.length));
+    return array[i];
+}
 function randomSide() {
-    var i =  Math.floor(Math.random() * (sides.length));
-   return sides[i]
+    var randSide = getRandomFood(sides);
+    alert (randSide)
 }
 
 function randomMain() {
-    var i = Math.floor(Math.random() * (mains.length));
-    return mains[i]
+    var randMain = getRandomFood(mains);
+    alert(randMain)
 }
 
 function randomDesert() {
-    var i = Math.floor(Math.random() * (deserts.length));
-    return deserts[i]
+    var randDesert = getRandomFood(deserts);
+    alert(randDesert)
 }
 
 // function entireMeal ()
 
 // I need a function that will give the output of the selected radio
 // button whe the lets cook button is clicked. 
-function getRandomFood() {
-    // var rando = randomDesert();
-    alert('This is what we found:' + randomDesert());
-    
-
-}
+// function getRandomFood() {
+//     // var rando = randomDesert();
+//     alert('This is what we found:' + randomDesert());
+// }
